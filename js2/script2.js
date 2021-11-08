d8eb415208459ed2bd229ece2c90121e321a7868
giulio30795 <giulioincarbone95@gmail.com>
2021-11-08 16:58:46 +0100

    snack2

--- js2/script2.js
+++ js2/script2.js
@@ -1,2 +1,8 @@
 // A partire da un array di numeri, genera un secondo array con le potenze al quadrato di ogni numero.
 // Es: [1, 2, 3, 4, 5] => [1, 4, 9, 16, 25]
+
+const array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const array2 = array1.map( element => element*element
+);
+console.log(array2, array1);
